afe75c3cd966506ab4228abe1878fff79b8c6e3d
mohammadiriaz <mohammadr95@nycstudents.net>
2025-09-25 10:15:25 -0400

    Update DogWalker.java

--- src/DogWalker.java
+++ src/DogWalker.java
@@ -7,3 +7,33 @@ public class DogWalker
         maxDogs = max;
         company = comp;
     }
+    public int walkDogs(int hour)
+    {
+        int dogs = company.numAvailableDogs(hour);
+        if (dogs < maxDogs)
+        {
+            company.updateDogs(hour, dogs);
+            return dogs;
+        }
+        else
+        {
+            company.updateDogs(hour, maxDogs);
+            return maxDogs;
+        }
+    }
+    public int dogWalkShift(int startHour, int endHour)
+    {
+        int pay = 0;
+        while(startHour <= endHour)
+        {
+            int dogs = walkDogs(startHour);
+            pay += dogs * 5;
+            if (dogs == maxDogs || startHour >= 9 && startHour <= 17)
+            {
+                pay += 3;
+            }
+            startHour++;
+        }
+        return pay;
+    }
+}
