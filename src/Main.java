7071a275f6d3d41c997c0b20ee554cc12dd02e02
mohammadiriaz <mohammadr95@nycstudents.net>
2025-09-25 10:15:49 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,3 +6,13 @@ public class Main
         int hour = c.addDogs();
         DogWalker w = new DogWalker(3, c);
         for (int i = 7; i < 11; i++)
+        {
+            c.addDogs();
+        }
+        System.out.println(w.walkDogs(hour) + " dogs walked.");
+        System.out.println(c.numAvailableDogs(hour) + " dogs remaining.");
+        System.out.println(c);
+        System.out.println("$" + w.dogWalkShift(7, 10) + ".00 earned");
+
+    }
+}
